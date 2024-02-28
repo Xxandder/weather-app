527bdddcf77213ca7e9302cb741153890d826344
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-28 16:59:37 +0200

    + empty daily forecast block

---
+++ weather-app/src/components/trips/daily-forecast/daily-forecast.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss';
+
+const DailyForecast: React.FC = () =>{
+  return <>
+        <div >
+            
+        </div>
+       
+  </>;
+}
+
+export  { DailyForecast };
