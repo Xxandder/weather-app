baf3a72b206fece8e54356a6c9080b0018285fa6
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-28 12:45:07 +0200

    * renamed property

--- weather-app/src/libs/constants/constants.ts
+++ weather-app/src/libs/constants/constants.ts
@@ -3,9 +3,9 @@ import warsawImage from '~/assets/images/berlin.jpg';
 import pragueImage from '~/assets/images/berlin.jpg';
 
 const cities = [
-    {city: 'Berlin', image: berlinImage},
-    {city: 'Warsaw', image: warsawImage},
-    {city: 'Prague', image: pragueImage}
+    {name: 'Berlin', image: berlinImage},
+    {name: 'Warsaw', image: warsawImage},
+    {name: 'Prague', image: pragueImage}
 ];
 
 export { cities };
