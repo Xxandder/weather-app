71c7133b4e386afdb4e645422fd8d1315535b9b4
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-28 12:43:29 +0200

    + cities constant

---
+++ weather-app/src/libs/constants/constants.ts
@@ -0,0 +1,11 @@
+import berlinImage from '~/assets/images/berlin.jpg';
+import warsawImage from '~/assets/images/berlin.jpg';
+import pragueImage from '~/assets/images/berlin.jpg';
+
+const cities = [
+    {city: 'Berlin', image: berlinImage},
+    {city: 'Warsaw', image: warsawImage},
+    {city: 'Prague', image: pragueImage}
+];
+
+export { cities };
