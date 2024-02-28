93318783dd0e19b1f6a057441aa24ceaed9eac1d
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-02-28 19:42:18 +0200

    + styles for DailyForecast

--- weather-app/src/components/daily-forecast/daily-forecast.tsx
+++ weather-app/src/components/daily-forecast/daily-forecast.tsx
@@ -3,19 +3,20 @@ import rainyIcon from '~/assets/icons/weather/rainy.svg';
 
 const DailyForecast: React.FC = () =>{
   return <div className={styles['daily-forecast']}>
-            <div className="daily-forecast__info">
-                <h2 className="daily-forecast__day">Sunday</h2>
-                <div className="daily-forecast__value">
-                    <div className="daily-forecast__weather-icon">
+            <div className={styles["daily-forecast__info"]}>
+                <h2 className={styles["daily-forecast__day"]}>Sunday</h2>
+                <div className={styles["daily-forecast__value"]}>
+                    <div className={styles["daily-forecast__weather-icon"]}>
                         <img src={rainyIcon} alt="" />
                     </div>
-                    <div className="daily-forecast__temperature">
-                        24<span>C</span>
+                    <div className={styles["daily-forecast__temperature"]}>
+                        24<sup>°C</sup>
                     </div>
-                    <div className="daily-forecast__city">
+                    
+                </div>
+                <div className={styles["daily-forecast__city"]}>
                         Berlin
                     </div>
-                </div>
             </div>
         </div>
 
