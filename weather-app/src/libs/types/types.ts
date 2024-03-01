1f1dbd12bbdb40c6b1480a6288d6dfe2bf9d6fd3
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-03-01 17:44:09 +0200

    + weather icon type

--- weather-app/src/libs/types/types.ts
+++ weather-app/src/libs/types/types.ts
@@ -1,3 +1,4 @@
 export { type TripData } from './trip-data.type.js';
 export { type FormData } from './modal-form-data.type.js';
 export { type DailyForecastData } from './daily-forecast-data.type.js';
+export { type WeatherIconType } from './weather-icon.type.js';
