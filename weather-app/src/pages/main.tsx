c371a0daa55909392bb98703a2878d04bfa3389e
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-03-01 18:06:17 +0200

    + updating current trip forecast when trip is clicked

--- weather-app/src/pages/main.tsx
+++ weather-app/src/pages/main.tsx
@@ -2,11 +2,10 @@ import { ChangeEvent, useCallback, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { DailyForecast, TripForecast, Trips, Modal } from '../components/components.js';
 import searchIcon from '~/assets/icons/search.svg';
-import { tripsForecast } from '../libs/constants/constants.js';
 import clsx from 'clsx'
 import styles from './styles.module.scss';
 import { cities, tripsList } from '../libs/constants/constants.js';
-import { type TripData, type FormData } from '~/libs/types/types.js';
+import { type TripData, type FormData, type DailyForecastData } from '~/libs/types/types.js';
 import { forecastApi } from '~/libs/api/forecast-api.js';
 
 
@@ -18,6 +17,8 @@ const MainPage: React.FC = () => {
     const [trips, setTrips] = useState<TripData[]>([...tripsList]);
 
     const [currentTrip, setCurrentTrip] = useState<number | null>(trips[0].id ?? null);
+
+    const [currentTripForecast, setCurrentTripForecast] = useState<DailyForecastData[]>([]);
 
     const handleInputOnChange = (event: ChangeEvent<HTMLInputElement>) => {
         setName(event.target.value);
@@ -42,10 +43,15 @@ const MainPage: React.FC = () => {
 
     const handleTripClick = useCallback(async (id: number) =>{
         setCurrentTrip(id);
-        if(currentTrip)
-        console.log(await forecastApi.getForecastForDaysRange(trips[currentTrip].startDate,
-            trips[currentTrip].endDate,trips[currentTrip].city.name));
-    }, [trips, currentTrip])
+        if(currentTrip){
+            const tripForecast = await forecastApi.getForecastForDaysRange(trips[currentTrip].startDate,
+                trips[currentTrip].endDate,trips[currentTrip].city.name);
+            if(tripForecast){
+                setCurrentTripForecast(tripForecast)
+            }
+        }
+            
+        }, [trips, currentTrip])
 
     return (
         <div className="_container">
@@ -60,7 +66,7 @@ const MainPage: React.FC = () => {
                             <input type="text" value={name} onChange={handleInputOnChange} placeholder='Search your trip'/>
                     </div>
                     <Trips activeId={currentTrip ?? 0} trips={trips} onAddTripButtonClick={toggleModal} onTripClicked={handleTripClick}/>
-                    <TripForecast tripForecast={tripsForecast}/>
+                    <TripForecast tripForecast={currentTripForecast}/>
                 </main>
                 <DailyForecast/>
                 
