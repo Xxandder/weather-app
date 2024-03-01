2093feab5e4ccb22039bc2fcdef6acf50179d976
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-03-01 17:38:59 +0200

    + enum for weather icon links

--- weather-app/src/libs/enums/enums.ts
+++ weather-app/src/libs/enums/enums.ts
@@ -1,1 +1,2 @@
-export { WeatherIcon } from './weather-icon-link.enum.js';
+export { WeatherIcon } from './weather-icon.enum.js';
+export { WeatherIconLink } from './weather-icon-link.enum.js';
--- weather-app/src/libs/enums/weather-icon-link.enum.ts
+++ weather-app/src/libs/enums/weather-icon-link.enum.ts
@@ -1,26 +1,28 @@
-const WeatherIcon = {
-    CLEAR_DAY: 'clear-day',
-    CLEAR_NIGHT: 'clear-night',
-    CLOUDY: 'cloudy',
-    FOG: 'fog',
-    HAIL: 'hail',
-    PARTLY_CLOUDY_DAY: 'partly-cloudy-day',
-    PARTLY_CLOUDY_NIGHT: 'partly-cloudy-night',
-    RAIN_SNOW_SHOWERS_DAY: 'rain-snow-showers-day',
-    RAIN_SNOW_SHOWERS_NIGHT: 'rain-snow-showers-night',
-    RAIN_SNOW: 'rain-snow',
-    RAIN: 'rain',
-    SHOWERS_DAY: 'showers-day',
-    SHOWERS_NIGHT: 'showers-night',
-    SLEET: 'sleet',
-    SNOW_SHOWERS_DAY: 'snow-showers-day',
-    SNOW_SHOWERS_NIGHT: 'snow-showers-night',
-    SNOW: 'snow',
-    THUNDER_RAIN: 'thunder-rain',
-    THUNDER_SHOWERS_DAY: 'thunder-showers-day',
-    THUNDER_SHOWERS_NIGHT: 'thunder-showers-night',
-    THUNDER: 'thunder',
-    WIND: 'wind'
+import { WeatherIcon } from "./weather-icon.enum"
+
+const WeatherIconLink = {
+    [WeatherIcon.CLEAR_DAY]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/clear-day.svg',
+    [WeatherIcon.CLEAR_NIGHT]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/clear-night.svg',
+    [WeatherIcon.CLOUDY]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/cloudy.svg',
+    [WeatherIcon.FOG]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/fog.svg',
+    [WeatherIcon.HAIL]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/hail.svg',
+    [WeatherIcon.PARTLY_CLOUDY_DAY]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/partly-cloudy-day.svg',
+    [WeatherIcon.PARTLY_CLOUDY_NIGHT]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/partly-cloudy-night.svg',
+    [WeatherIcon.RAIN_SNOW_SHOWERS_DAY]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/rain-snow-showers-day.svg',
+    [WeatherIcon.RAIN_SNOW_SHOWERS_NIGHT]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/rain-snow-showers-night.svg',
+    [WeatherIcon.RAIN_SNOW]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/rain-snow.svg',
+    [WeatherIcon.RAIN]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/rain.svg',
+    [WeatherIcon.SHOWERS_DAY]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/showers-day.svg',
+    [WeatherIcon.SHOWERS_NIGHT]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/showers-night.svg',
+    [WeatherIcon.SLEET]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/sleet.svg',
+    [WeatherIcon.SNOW_SHOWERS_DAY]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/snow-showers-day.svg',
+    [WeatherIcon.SNOW_SHOWERS_NIGHT]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/snow-showers-night.svg',
+    [WeatherIcon.SNOW]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/snow.svg',
+    [WeatherIcon.THUNDER_RAIN]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/thunder-rain.svg',
+    [WeatherIcon.THUNDER_SHOWERS_DAY]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/thunder-showers-day.svg',
+    [WeatherIcon.THUNDER_SHOWERS_NIGHT]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/thunder-showers-night.svg',
+    [WeatherIcon.THUNDER]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/thunder.svg',
+    [WeatherIcon.WIND]: 'https://github.com/visualcrossing/WeatherIcons/blob/main/SVG/1st%20Set%20-%20Color/wind.svg'
 } as const;
 
-export { WeatherIcon };
+export { WeatherIconLink };
